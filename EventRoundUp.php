4c2f840e63e729265fa1ee2f76584ea5a43cdc0d
James Strocel <James.Strocel@gmail.com>
2012-04-24 15:03:22 -0700

    Added evcru_create_menu and evcru_settings_link

--- EventRoundUp.php
+++ EventRoundUp.php
@@ -24,4 +24,27 @@ Author URI: http://www.v2swebdesign.com
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+
+function evcru_create_menu() {
+
+  //create new top-level menu
+  add_options_page('Event Calendar Round Up', 'Event Calendar Round Up', 'manage_options', __FILE__, 'evcru_settings_page');
+  add_filter( "plugin_action_links", "evcru_settings_link", 10, 2 );
+  //call register settings function
+  add_action( 'admin_init', 'evcru_register_settings' );
+}
+
+//add settings link to plugins list
+function evcru_settings_link($links, $file) {
+  static $this_plugin;
+  if (!$this_plugin) $this_plugin = plugin_basename(__FILE__);
+  if ($file == $this_plugin){
+    $settings_link = '<a href="options-general.php?page=EventRoundUp/EventRoundUp.php">'.__("Settings", "EventRoundUp").'</a>';
+    array_unshift($links, $settings_link);
+  }
+  return $links;
+  }
+
+
 ?>
