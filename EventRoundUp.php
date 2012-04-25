b3ce78998e858b79f4a784b8c07a4f594db61c92
James Strocel <James.Strocel@gmail.com>
2012-04-24 22:54:51 -0700

    Plugin can now create posts every 30 seconds

--- EventRoundUp.php
+++ EventRoundUp.php
@@ -26,6 +26,8 @@ Author URI: http://www.v2swebdesign.com
 */
 
 
+add_action('admin_menu', 'evcru_create_menu');
+
 function evcru_create_menu() {
 
   //create new top-level menu
@@ -34,6 +36,57 @@ function evcru_create_menu() {
   //call register settings function
   add_action( 'admin_init', 'evcru_register_settings' );
 }
+
+
+
+#register_activation_hook(__FILE__, 'my_activation');
+add_action('wp', 'my_activation');
+add_action('my_hourly_event', 'do_this_hourly');
+
+add_filter( 'cron_schedules', 'cron_add_seconds' );
+ 
+ function cron_add_seconds( $schedules ) {
+ 	// Adds once weekly to the existing schedules.
+ 	$schedules['seconds'] = array(
+ 		'interval' => 30,
+ 		'display' => __( 'seconds' )
+ 	);
+ 	return $schedules;
+ }
+
+
+
+function my_activation() {
+  $activated = get_option('evcru_activated');
+  if ($activated == true){
+    if ( !wp_next_scheduled( 'my_hourly_event' ) ) {
+	    wp_schedule_event( current_time( 'timestamp' ), 'seconds', 'my_hourly_event');
+    }
+  }
+}
+
+register_deactivation_hook(__FILE__, 'my_deactivation');
+
+function my_deactivation() {
+	wp_clear_scheduled_hook('my_hourly_event');
+}
+
+
+function do_this_hourly() {
+	// do something every hour
+  // Create post object
+    $my_post = array(
+       'post_title' => 'My post',
+       'post_content' => 'This is my post.'.current_time( 'timestamp' ),
+       'post_status' => 'publish',
+       'post_author' => 1,
+       'post_category' => array(8,39)
+    );
+  // Insert the post into the database
+    wp_insert_post( $my_post );
+}
+
+
 
 //add settings link to plugins list
 function evcru_settings_link($links, $file) {
@@ -45,6 +98,81 @@ function evcru_settings_link($links, $file) {
   }
   return $links;
   }
+  
+  function evcru_register_settings() {
+    //register our settings
+    register_setting( 'EventRoundUp', 'evcru_calendar_url' );
+    register_setting( 'EventRoundUp', 'evcru_interval' );
+    register_setting( 'EventRoundUp', 'evcru_day' );	
+    register_setting( 'EventRoundUp', 'evcru_activated' );
+  }
+  
+  function evcru_admin_css() { ?>
+  <style type="text/css"  >
 
+  .evcru_social_list {padding-top:15px;}
+  .evcru_social_list .setting {display:block;padding:1em;}
+  .evcru_social_list .setting p.label_title {font-size:12px;font-weight:bold;display:block;margin-bottom:5px;}
+  .evcru_social_list .setting label.no_bold {font-weight:normal;}
+  .evcru_social_list .setting label span.slim {width:200px;float:left;display:block;margin: 1px;padding: 3px;}
+  .evcru_social_list .setting p.desc {font-size:10px;font-style:italic;text-indent:10px; text-align:left;}
+  </style>
+  <?php }
+  add_action('admin_head', 'evcru_admin_css');
+  
+  //html for settings form
+  function evcru_settings_page() { ?>
+    
+<?php    
+
+
+if ( isset($_POST['submit']) ) {
+
+if ( isset( $_POST['evcru_activated'] ) )
+			update_option( 'evcru_activated', 'true' );
+		else
+			update_option( 'evcru_activated', 'false' );
+			
+			}?>
+
+  <div class="wrap evcru_social_list">
+    <h2>Event Calendar Round Up</h2>
+
+    <form method="post" action="options.php">
+      <?php settings_fields( 'EventRoundUp' ); ?>
+
+
+          <div class="setting">
+
+          <p class="label_title">Where's the Calendar?'</p>
+          <p><label class="no_bold" for="evcru_calendar_url"><span class="slim">Calendar URL</span>
+          <input name="evcru_calendar_url" type="text" id="evcru_facebook" value="<?php form_option('evcru_calendar_url'); ?>" /></label></p>
+          
+          <p class="label_title">How Often?</p>
+          <p><label class="no_bold" for="evcru_interval"><span class="slim">Interval</span>
+          <input name="evcru_interval" type="text" id="evcru_interval" value="<?php form_option('evcru_interval'); ?>" /></label></p>
+        
+
+          <p class="label_title">What Day?</p>
+          <p><label class="no_bold" for="evcru_day"><span class="slim">Day of Post</span>
+          <input name="evcru_day" type="text" id="evcru_day" value="<?php form_option('evcru_day'); ?>" /></label></p>
+          
+          <p class="label_title">Activate?</p>
+          <p><label class="no_bold" for="evcru_activated"><span class="slim">Activated?</span>
+            <input name="evcru_activated" type="checkbox" value="1" <?php checked( '1', get_option( 'evcru_activated' ) ); ?> />
+          
+      
+          <p class="setting">
+          <input type="submit" class="button-primary" value="Save Settings" />
+          </p>
+
+          </div>
+
+    </form>
+
+  </div>
+
+  <?php }
+  
 
 ?>
